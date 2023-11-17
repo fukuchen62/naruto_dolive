de1611a3a35f06f69ecedab0945d0a6ede58d43d
kengo-kawabata <web.train.toku@gmail.com>
2023-11-17 15:52:38 +0900

    マイページのお気に入りの地図の機能の実装、テスト
    マイページのお気に入りの地図の機能の実装、テストをしました。
    それに伴い、少しだけcssを記載しました。
    
    川端

--- page-mypage.php
+++ page-mypage.php
@@ -10,6 +10,8 @@ get_header();
 
 <h2>マイページです</h2>
 
+<?php get_sidebar('categories'); ?>
+
 <?php the_content(); ?>
 
 
@@ -32,13 +34,78 @@ get_header();
                 <p><?php echo get_the_content(); ?></p>
                 <p><?php the_field('address'); ?></p>
 
+                <?php
+                // 緯度と経度の値を取得する
+                $latitude = get_post_meta(get_the_ID(), 'latitude', true);
+                $longitude = get_post_meta(get_the_ID(), 'longitude', true);
+                $facilityName = get_the_title();
+
+                $marker_locations[] = array('lat' => $latitude, 'lng' => $longitude, 'facilityName' => $facilityName);
+                ?>
+
+                <p>Latitude: <?php echo $latitude; ?></p>
+                <p>Longitude: <?php echo $longitude; ?></p>
+                <p>施設名: <?php echo $facilityName; ?></p>
             </article>
         <?php endwhile; ?>
     <?php endif; ?>
     <?php wp_reset_postdata(); ?>
 </div>
 
+
+
+
+
+
+<div id="map"></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCpvX7RaM_ofrk2WWXrnydT9156PzLJBuA&callback=initMap" async defer></script>
+<script>
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 12,
+            center: {
+                lat: <?php echo $latitude; ?>,
+                lng: <?php echo $longitude; ?>
+            }
+        });
+
+        var markerLocations = <?php echo json_encode($marker_locations); ?>;
+        console.log(markerLocations);
+
+        // マーカーを表示するループ
+        for (var i = 0; i < markerLocations.length; i++) {
+            var location = markerLocations[i];
+
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: parseFloat(location.lat),
+                    lng: parseFloat(location.lng)
+                },
+                map: map,
+                title: 'test'
+            });
+
+            // インフォウィンドウを作成してマーカーに設定
+            var infowindow = new google.maps.InfoWindow({
+                content: location.facilityName
+            });
+
+            // マーカー上でインフォウィンドウを開く
+            infowindow.open(map, marker);
+        }
+    }
+</script>
+
+
+
+
+
 <a href="<?php echo home_url(''); ?>">ホームへ</a><br>
+
+
+
+
+
 
 
 <?php
