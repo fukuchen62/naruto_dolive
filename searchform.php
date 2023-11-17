682488bcc8019d472496ac1a9f047647d66fe11e
kawaguchi-miho <hekoumiho@gmail.com>
2023-11-17 09:59:25 +0900

    searchform.phpを更新しました
    更新しました。
    
    河口

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form action="<?php echo home_url('/'); ?>" method="get" class="header_search">
+    <input type="text" name="s" value="<?php the_search_query(); ?>" placeholder="文字列検索">
+    <i class="fas fa-search"></i>
+</form>
