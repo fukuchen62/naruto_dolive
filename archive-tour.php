9fdf0680794366551a406736bf610916278aeae0
yukanishi1216 <yuka.nishi0606@gmail.com>
2023-11-20 11:41:18 +0900

    食べる・遊ぶ・宿泊・観光一覧ページを変更しました。
    食べる・遊ぶ・宿泊・観光一覧ページを変更しました。
    
    西脇

--- archive-tour.php
+++ archive-tour.php
@@ -1,11 +1,55 @@
 <?php get_header(); ?>
-
-<!-- パンくずリスト -->
-<?php get_template_part('template-parts/breadcrumb'); ?>
-
-
 <main>
+    <h1>========『目的別一覧』========</h1>
+    <!-- パンくずリスト -->
+    <?php get_template_part('template-parts/breadcrumb'); ?>
     <h1>【観光】</h1>
+
+    <p>[ここに地図が入ります]</p>
+    <p>カテゴリ別</p>
+    <h3>
+        <a href="<?php echo home_url('/eat') ?>">
+            食べる(
+            <?php
+            $count_custom = wp_count_posts('eat');
+            $num = $count_custom->publish;
+            echo $num; ?>件 )
+        </a>
+    </h3>
+
+    <h3>
+        <a href="<?php echo home_url('/enjoy') ?>">
+            遊ぶ(
+            <?php
+            $count_custom = wp_count_posts('enjoy');
+            $num = $count_custom->publish;
+            echo $num; ?>件 )
+        </a>
+    </h3>
+
+    <h3>
+        <a href="<?php echo home_url('/tour') ?>">
+            観光(
+            <?php
+            $count_custom = wp_count_posts('tour');
+            $num = $count_custom->publish;
+            echo $num; ?>件 )
+        </a>
+    </h3>
+
+    <h3>
+        <a href="<?php echo home_url('/stay') ?>">
+            宿泊(
+            <?php
+            $count_custom = wp_count_posts('stay');
+            $num = $count_custom->publish;
+            echo $num; ?>件 )
+        </a>
+    </h3>
+
+
+
+
 
     <!-- タクソノミーを指定して配列のターム情報を取得する -->
     <!-- タクソノミーのタイトルの取得 -->
