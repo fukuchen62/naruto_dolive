bba2dfe9fae94fc2239c72f377c579e1a11e6b13
kawaguchi-miho <hekoumiho@gmail.com>
2023-11-22 16:03:48 +0900

    2ファイル変更
    2ファイル、ページネーション追加しました。
    
    河口

--- archive-column.php
+++ archive-column.php
@@ -30,162 +30,159 @@ get_header();
                     <?php endif; ?>
 
                 </div>
-                <?php if (function_exists('wp_pagenavi')) : ?>
-                    <div class="pagination">
-                        <?php wp_pagenavi(); ?>
-                    </div>
 
-                <?php endif ?>
+
             </div>
+            <hr>
+            <?php the_posts_pagination(
+                array(
+                    'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
+                    'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
+                    'prev_text'     => ('◀'), // 「前へ」リンクのテキスト
+                    'next_text'     => ('▶'), // 「次へ」リンクのテキスト
+                    'type'          => 'plain', // 戻り値の指定 (plain/list)
+                )
 
-            <!-- ページナビゲーションの設定 -->
-            <style>
-                /*中央に配置*/
-                .wp-pagenavi {
-                    text-align: center;
-                }
-
-                /*ページボタンの共通スタイル*/
-                .wp-pagenavi span.current,
-                .page.smaller,
-                .page.larger {
-                    display: inline-block;
-                    width: 50px;
-                    height: 50px;
-                    text-align: center;
-                    line-height: 50px;
-                    padding: 0;
-                    background: #000;
-                    color: #fff;
-                    /* background: none; */
-                    /* 背景をクリアする */
-                    /* color: transparent; */
-                    /* テキストを透明にする */
-                    border-radius: 50%;
-                    border: none;
-                }
+            ); ?>
 
 
-
-                /*現在ページ以外のボタン*/
-                /* 共通スタイル*/
-                .page.smaller,
-                .page.larger {
-                    background: #fff;
-
-                    color: #000;
-                    border: 1px solid #000;
-                    padding: 0;
-                }
-
-                /* 現在ページより前のボタン */
-                .page.smaller {
-                    margin-right: 25px;
-                }
-
-                /* 現在ページより後のボタン */
-                .page.larger {
-                    margin-left: 25px;
-                }
-
-
-                /*マウスが乗った時の、ボタンの背景の色と文字の色*/
-                .wp-pagenavi a:hover {
-                    opacity: 0.5;
-                }
-
-                /*次へ, 前へ 共通スタイル*/
-                .nextpostslink,
-                .previouspostslink {
-                    line-height: 40px;
-                    border: 1px solid #fff;
-                    color: #000;
-                    position: relative;
-                    background: #fff;
-                    color: #000;
-                    top: 15px;
-                    font-size: 50px;
-                    /* 追加 */
-                    width: 50px;
-                    /* 追加 */
-                    height: 50px;
-                    /* 追加 */
-                    text-align: center;
-                    /* 追加 */
-                    line-height: 50px;
-                    /* 追加 */
-                    border-radius: 50%;
-                    /* 追加 */
-
-                }
-
-                /*次へ*/
-                .nextpostslink {
-                    left: 25px;
-                }
-
-                /*前へ*/
-                .previouspostslink {
-                    right: 25px;
-                }
-
-                /* 〇/〇 の部分のスタイル → 不要の場合は非表示 */
-                .pages {
-                    display: none;
-                }
-
-                /* 画像のスタイル */
-                /* .wp-pagenavi span.current::before,
-                .page.smaller::before,
-                .page.larger::before {
-                    content: url('/wp-content/themes/naruto_dolive/images/1.png'); */
-                /* 画像を設定する */
-                /* display: inline-block;
-                    width: 100%;
-                    height: 100%;
-                } */
-
-                .wp-pagenavi span.current {
-                    display: inline-block;
-                    background: url("http://localhost:8888/naruto_dolive/wp-content/themes/naruto_dolive/assets/img/1.png");
-                    /* background: url("naruto_dolive/wp-content/themes/naruto_dolive/assets/img/1.png"); */
-                    background-size: 50px 50px;
-                    /* 背景画像の大きさを指定 */
-                    background-position: center;
-                    /* 背景画像の配置を中央に指定 */
-                    color: transparent;
-                    text-indent: -9999px;
-                }
-            </style>
-
-            <!-- サイドバー -->
-            <h2>以下サイドバーです</h2>
 
         </div>
-
-        <aside>
-            <div>
-                <h3>カテゴリー一覧</h3>
-                <ul>
-                    <?php
-                    $categories = get_categories(array(
-                        'taxonomy' => 'column_type',
-                        'orderby' => 'name',
-                        'order' => 'ASC',
-                        'hide_empty' => false,
-                    ));
-
-                    foreach ($categories as $category) {
-                        $category_link = get_category_link($category->term_id);
-                        $post_count = $category->count;
-                        echo '<li><a href="' . $category_link . '">' . $category->name . '</a> (' . $post_count . ')</li>';
-                    }
-                    ?>
-                </ul>
-            </div>
+        <!-- ページナビゲーションの設定 -->
 
 
-        </aside>
+        <style>
+            /*真ん中に設置*/
+            .pagination {
+                text-align: center;
+
+            }
+
+            .pagenation-page {
+                text-align: center;
+
+            }
+
+            /*横並びにする*/
+            .nav-links {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+
+            }
+
+            .page-numbers {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                position: relative;
+
+            }
+
+            /*ページタブの共通スタイル*/
+            .nav-links a.page-numbers,
+            .nav-links .current,
+            .nav-links a.prev,
+            .nav-links a.next,
+            .nav-links .dots {
+                width: 70px;
+                height: 60px;
+                text-align: center;
+                line-height: 50px;
+                background: #fff;
+                /* color: #7090DD; */
+                /* border-radius: 50%; */
+                margin-right: 25px;
+                font-size: 1.5rem;
+                font-weight: bold;
+                /* border: 1px solid #7090DD; */
+                padding-top: 10px;
+                text-decoration: none;
+                color: #000;
+
+            }
+
+            .nav-links a.page-numbers,
+            .nav-links .current {
+                background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/pagination.png);
+                background-size: 100%;
+                font-family: 'Barlow Condensed', sans-serif;
+            }
+
+            .nav-links a.prev,
+            .nav-links a.next,
+            .nav-links .dots {
+                background-image: none;
+                font-size: 3rem;
+
+            }
+
+
+            /*マウスが乗った時の、ボタンの背景の色と文字の色*/
+            .nav-links a.page-numbers:hover {
+                color: #FFF;
+                background-color: #999;
+                /* border-color: #999; */
+                border-radius: 50%;
+
+            }
+
+            /*現在のページのタブ*/
+            .nav-links .current {
+                /* background: #7090DD; */
+                color: #000;
+                padding: 0px;
+                padding-top: 10px;
+            }
+
+
+            /*「次へ」の前の縦線位置*/
+            .nav-links .next::before {
+                left: -15px;
+                display: none;
+            }
+
+            /*前へ」の後の縦線位置*/
+            .nav-links .prev::after {
+                right: -15px;
+                display: none;
+            }
+
+            /*数字省略の「・・・」*/
+            .nav-links .dots {
+                background: transparent;
+                border: none;
+            }
+        </style>
+
+        <!-- サイドバー -->
+        <h2>以下サイドバーです</h2>
+
+    </div>
+
+    <aside>
+        <div>
+            <h3>カテゴリー一覧</h3>
+            <ul>
+                <?php
+                $categories = get_categories(array(
+                    'taxonomy' => 'column_type',
+                    'orderby' => 'name',
+                    'order' => 'ASC',
+                    'hide_empty' => false,
+                ));
+
+                foreach ($categories as $category) {
+                    $category_link = get_category_link($category->term_id);
+                    $post_count = $category->count;
+                    echo '<li><a href="' . $category_link . '">' . $category->name . '</a> (' . $post_count . ')</li>';
+                }
+                ?>
+            </ul>
+        </div>
+
+
+    </aside>
 
 
 
