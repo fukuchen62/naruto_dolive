1d64a6792c633dbf74f9811d812b5b575b51a5f5
kengo-kawabata <web.train.toku@gmail.com>
2023-11-28 12:05:38 +0900

    フッター関連編集
    フッター関連編集しました。
    
    川端

--- footer.php
+++ footer.php
@@ -1,5 +1,11 @@
 <footer>
-
+    <p><a href="<?php echo get_permalink(718) ?>">このサイトについて</a></p>
+    <p><a href="#">サイトマップ</a></p>
+    <p><a href="<?php echo get_permalink(3) ?>">プライバシーポリシー</a></p>
+    <p class="copyright"><small>&copy; 2023 Team藍 in QLIP</small></p>
+    <div>
+        <p id="page_top"><a href="#"><span>Page Top</span></a></p>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
