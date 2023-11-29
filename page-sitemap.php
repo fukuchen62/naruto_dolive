56fa90b7a7276f371bff1da0b2c24a7811247581
KengoTakahashi11 <tkkm1126@gmail.com>
2023-11-29 09:32:56 +0900

    sitemap.phpとcss変更
    sitemap.phpとcss変更
    
    高橋　賢吾

--- page-sitemap.php
+++ page-sitemap.php
@@ -6,144 +6,135 @@ get_header();
 ?>
 
 <?php get_template_part('template-parts/breadcrumb'); ?>
-<h2>sitemap.php</h2>
 
-<h2>新着情報一覧</h2>
-<?php the_content(); ?>
-<div class="row">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <div>
-                <?php get_template_part('template-parts/loop', 'news'); ?>
-            </div>
-            <hr>
-        <?php endwhile; ?>
+
+<main>
+    <section id="toparea" class="toparea">
+        <h2>サイトマップ</h2>
+    </section>
+    <div class="main_wrap">
+        <div class="breadcrumb">HOME > サイトマップ</div>
+        <div class="menu_wrap">
+            <h3 class="sitemap_btn"><span>TOP</span></h3>
+            <h3 class="sitemap_btn"><span>コースで探す</span></h3>
+            <ul class="sitemap_list">
+                <?php
+                $args = array(
+                    'post_type' => 'cource',
+                    'posts_per_page' => -1 // 全ての投稿を表示
+                );
+
+                $the_query = new WP_Query($args);
+
+                if ($the_query->have_posts()) {
+                    echo '<ul>';
+                    while ($the_query->have_posts()) {
+                        $the_query->the_post();
+                        echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+                    }
+                    echo '</ul>';
+                }
+
+                ?>
+
+                <li><a href="">Aコース</a></li>
+                <li><a href="#">Bコース</a></li>
+                <li><a href="#">Cコース</a></li>
+                <li><a href="#">Dコース</a></li>
+                <li><a href="#">Eコース</a></li>
+            </ul>
+            <h3 class="sitemap_btn"><span>目的で探す</span></h3>
+            <h4><a href="<?php echo home_url('/eat') ?>">食べる</a></h4>
+            <?php $eat_types = get_terms(array('taxonomy' => 'eat_type'));
+            if (!empty($eat_types)) : ?>
+                <ul class="sitemap_list">
+                    <?php foreach ($eat_types as $eat_type) : ?>
+                        <li><a href="<?php echo esc_url(get_term_link($eat_type)); ?>"><?php echo esc_html($eat_type->name); ?></a></li>
+
+
+                        <!-- <li><a href="#">洋食</a></li>
+                        <li><a href="#">中華</a></li>
+                        <li><a href="#">スイーツ</a></li>
+                        <li><a href="#">その他</a></li> -->
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            </ul>
+            <h4><a href="<?php echo home_url('/enjoy') ?>">遊ぶ</a></h4>
+            <h4><a href="<?php echo home_url('/tour') ?>">観光</a></h4>
+            <h4><a href="<?php echo home_url('/stay') ?>">宿泊</a></h4>
+            <ul class="sitemap_list">
+                <?php $tour_types = get_terms(array('taxonomy' => 'tour_type'));
+                if (!empty($tour_types)) : ?>
+                    <?php foreach ($tour_types as $tour_type) : ?>
+                        <li><a href="<?php echo esc_url(get_term_link($tour_type)); ?>"><?php echo esc_html($tour_type->name); ?></a></li>
+
+
+
+                    <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        <!-- <li><a href="#">自然</a></li>
+                <li><a href="#">アート・カルチャー</a></li>
+                <li><a href="#">歴史・文化</a></li>
+                <li><a href="#">寺院・神社</a></li>
+                <li><a href="#">公園</a></li> -->
+        </ul>
+        <h3 class="sitemap_btn"><span>新着情報</span></h3>
+        <ul class="sitemap_list">
+            <?php
+            $categories = get_categories(array(
+                'type'      => 'post',
+                'orderby'   => 'name',
+                'order'     => 'ASC'
+            ));
+
+            if (!empty($categories)) :
+
+                foreach ($categories as $category) : ?>
+                    <li><a href="<?php echo esc_url(get_category_link($category->term_id)); ?>"><?php echo esc_html($category->name); ?></a></li>
+
+                <?php endforeach; ?>
+        </ul>
     <?php endif; ?>
+    <h3 class="sitemap_btn"><span>コラム</span></h3>
+    <ul class="sitemap_list">
+
+        <?php $column_types = get_terms(array('taxonomy' => 'column_type', 'orderby' => 'name', 'order' => 'desc'));
+        if (!empty($column_types)) : ?>
+
+            <?php foreach ($column_types as $column_type) : ?>
+                <li><a href="<?php echo esc_url(get_term_link($column_type)); ?>"><?php echo esc_html($column_type->name); ?></a></li>
+
+
+
+            <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+
+<ul class="sitemap_list u_content">
+    <li><a href="#">インスタ</a></li>
+    <li><a href="<?php echo home_url('/mypage') ?>">マイページ</a></li>
+    <li><a href="<?php echo home_url('/q_a') ?>">Q &amp; A</a></li>
+</ul>
+        </div>
+
+
+    </div>
+</main>
+<footer>
+    <p><a href="#">このサイトについて</a></p>
+    <p><a href="#">サイトマップ</a></p>
+    <p><a href="#">プライバシーポリシー</a></p>
+    <p class="copyright"><small>&copy; 2023 Team藍 in QLIP</small></p>
+</footer>
+<div class="button">
 </div>
-<div>
+<script src="../assets/js/common.js"></script>
+</body>
 
-    <?php the_posts_pagination(
-        array(
-            'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
-            'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
-            'prev_text'     => ('◀'), // 「前へ」リンクのテキスト
-            'next_text'     => ('▶'), // 「次へ」リンクのテキスト
-            'type'          => 'plain', // 戻り値の指定 (plain/list)
-        )
-
-    ); ?>
-</div>
-<hr>
-<div>
-    <style>
-        /*真ん中に設置*/
-        .pagination {
-            text-align: center;
-
-        }
-
-        .pagenation-page {
-            text-align: center;
-
-        }
-
-        /*横並びにする*/
-        .nav-links {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-
-        }
-
-        .page-numbers {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            position: relative;
-
-        }
-
-        /*ページタブの共通スタイル*/
-        .nav-links a.page-numbers,
-        .nav-links .current,
-        .nav-links a.prev,
-        .nav-links a.next,
-        .nav-links .dots {
-            width: 70px;
-            height: 60px;
-            text-align: center;
-            line-height: 50px;
-            background: #fff;
-            /* color: #7090DD; */
-            /* border-radius: 50%; */
-            margin-right: 25px;
-            font-size: 1.5rem;
-            font-weight: bold;
-            /* border: 1px solid #7090DD; */
-            padding-top: 10px;
-            text-decoration: none;
-            color: #000;
-        }
-
-        .nav-links a.page-numbers,
-        .nav-links .current {
-            background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/pagination.png);
-            background-size: 100%;
-            font-family: 'Barlow Condensed', sans-serif;
-        }
-
-        .nav-links a.prev,
-        .nav-links a.next,
-        .nav-links .dots {
-            background-image: none;
-            font-size: 3rem;
-
-        }
-
-
-        /*マウスが乗った時の、ボタンの背景の色と文字の色*/
-        .nav-links a.page-numbers:hover {
-            color: #FFF;
-            background-color: #999;
-            /* border-color: #999; */
-            border-radius: 50%;
-
-        }
-
-        /*現在のページのタブ*/
-        .nav-links .current {
-            /* background: #7090DD; */
-            color: #000;
-            padding: 0px;
-            padding-top: 10px;
-        }
-
-
-        /*「次へ」の前の縦線位置*/
-        .nav-links .next::before {
-            left: -15px;
-            display: none;
-        }
-
-        /*前へ」の後の縦線位置*/
-        .nav-links .prev::after {
-            right: -15px;
-            display: none;
-        }
-
-        /*数字省略の「・・・」*/
-        .nav-links .dots {
-            background: transparent;
-            border: none;
-        }
-    </style>
-</div>
-<h2>サイドバーです</h2>
-<!-- サイドバー出力 -->
-<?php get_sidebar('categories'); ?>
-
-<a href="<?php echo home_url(''); ?>">ホームへ</a>
-
+</html>
 
 
 <?php
