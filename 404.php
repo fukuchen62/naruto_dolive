efa229ae1afbd449b59448d4736fca3bbe705e3a
KengoTakahashi11 <tkkm1126@gmail.com>
2023-11-29 11:13:38 +0900

    404.php編集
    404.php編集
    
    高橋　賢吾

---
+++ 404.php
@@ -0,0 +1,21 @@
+<body>
+    <div id="stkr" class="sp_none"><img src="../assets/img/maincar_icon.png" alt="" class="car_img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/exhaust_fumes.gif" alt="" class="gas"></div>
+    <?php get_header(); ?>
+    <section id="toparea" class="toparea">
+        <h2>404NotFound</h2>
+    </section>
+    <div class="main_wrap">
+        <div class="breadcrumb">HOME > 404</div>
+        <div class="center_title">
+            <p class="middle_title">お探しのページが見つかりません...</p>
+            <p>お探しのページが見つかりませんでした。<br>お手数ですが、<a href=""><u>こちらのリンク</u></a>からTOPページにお戻りください。</p>
+        </div>
+        <div class="image-container">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/not-found.png" alt="ページが見つからなかったときの画像">
+        </div>
+    </div>
+    <?php get_footer(); ?>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
+</body>
+
+</html>
