058c7ef9238fd9438c85e63af63b723fe10ff6a8
kawaguchi-miho <hekoumiho@gmail.com>
2023-12-01 17:32:08 +0900

    6ファイル更新。
    6ファイル更新しました。
    
    河口

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header();
                     <div class="card2_wrap">
 
                         <div class="card2_content">
-                            <h4>subtitle</h4>
+                            <h4></h4>
                             <br>
                             <div class="card2_img">
 
@@ -28,7 +28,7 @@ get_header();
 
                             </div>
 
-                            <div class="card1_text"><?php the_content(); ?></div>
+                            <div class="card2_text"><?php the_content(); ?></div>
                         </div>
                     </div>
                     <div class="pageturn">
--- template-parts/loop-column.php
+++ template-parts/loop-column.php
@@ -21,7 +21,7 @@
                 if ($terms && !is_wp_error($terms)) {
                     echo '<ul>';
                     foreach ($terms as $term) {
-                        echo '<li><a href="' . get_term_link($term) . '">' . $term->name . '</a></li>';
+                        echo '<li>' . $term->name . '</li>';
                     }
                     echo '</ul>';
                 }
