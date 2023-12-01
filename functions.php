5636dfc0ef0aa71e6c0c57c260fb271c69e36a44
yukanishi1216 <yuka.nishi0606@gmail.com>
2023-12-01 12:06:54 +0900

    Update functions.php
    functions.php変更しました
    
    西脇

--- functions.php
+++ functions.php
@@ -316,3 +316,133 @@ function my_theme_setup()
     add_theme_support('menus');
 }
 add_action('after_setup_theme', 'my_theme_setup');
+
+
+
+// サイト内検索でカスタムフィールドの内容も検索結果に含める
+add_filter('posts_search', 'custom_search_eat', 10, 2);
+
+function custom_search_eat($search, $wp_query)
+{
+    global $wpdb;
+    if (!$wp_query->is_search)
+        return $search;
+    if (!isset($wp_query->query_vars))
+        return $search;
+
+    $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+
+    if (count($search_words) > 0) {
+        $search = '';
+        $search .= "AND post_type = 'eat','tour'";
+        foreach ($search_words as $word) {
+            if (!empty($word)) {
+                $search_word = '%' . esc_sql($word) . '%';
+                $search .= " AND (
+                    {$wpdb->posts}.post_title LIKE '{$search_word}'
+                    OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+                    OR {$wpdb->posts}.ID IN (
+                    SELECT distinct post_id
+                    FROM {$wpdb->postmeta}
+                    WHERE meta_value LIKE '{$search_word}')) ";
+            }
+        }
+    }
+    return $search;
+}
+
+// // サイト内検索でカスタムフィールドの内容も検索結果に含める
+// add_filter('posts_search', 'custom_search_enjoy', 10, 2);
+
+// function custom_search_enjoy($search, $wp_query)
+// {
+//     global $wpdb;
+//     if (!$wp_query->is_search)
+//         return $search;
+//     if (!isset($wp_query->query_vars))
+//         return $search;
+
+//     $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+
+//     if (count($search_words) > 0) {
+//         $search = '';
+//         $search .= "AND post_type = 'enjoy'";
+//         foreach ($search_words as $word) {
+//             if (!empty($word)) {
+//                 $search_word = '%' . esc_sql($word) . '%';
+//                 $search .= " AND (
+//                     {$wpdb->posts}.post_title LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.ID IN (
+//                     SELECT distinct post_id
+//                     FROM {$wpdb->postmeta}
+//                     WHERE meta_value LIKE '{$search_word}')) ";
+//             }
+//         }
+//     }
+//     return $search;
+// }
+
+// // サイト内検索でカスタムフィールドの内容も検索結果に含める
+// add_filter('posts_search', 'custom_search_tour', 10, 2);
+
+// function custom_search_tour($search, $wp_query)
+// {
+//     global $wpdb;
+//     if (!$wp_query->is_search)
+//         return $search;
+//     if (!isset($wp_query->query_vars))
+//         return $search;
+
+//     $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+
+//     if (count($search_words) > 0) {
+//         $search = '';
+//         $search .= "AND post_type = 'tour'";
+//         foreach ($search_words as $word) {
+//             if (!empty($word)) {
+//                 $search_word = '%' . esc_sql($word) . '%';
+//                 $search .= " AND (
+//                     {$wpdb->posts}.post_title LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.ID IN (
+//                     SELECT distinct post_id
+//                     FROM {$wpdb->postmeta}
+//                     WHERE meta_value LIKE '{$search_word}')) ";
+//             }
+//         }
+//     }
+//     return $search;
+// }
+
+// // サイト内検索でカスタムフィールドの内容も検索結果に含める
+// add_filter('posts_search', 'custom_search_stay', 10, 2);
+
+// function custom_search_stay($search, $wp_query)
+// {
+//     global $wpdb;
+//     if (!$wp_query->is_search)
+//         return $search;
+//     if (!isset($wp_query->query_vars))
+//         return $search;
+
+//     $search_words = explode(' ', isset($wp_query->query_vars['s']) ? $wp_query->query_vars['s'] : '');
+
+//     if (count($search_words) > 0) {
+//         $search = '';
+//         $search .= "AND post_type = 'stay'";
+//         foreach ($search_words as $word) {
+//             if (!empty($word)) {
+//                 $search_word = '%' . esc_sql($word) . '%';
+//                 $search .= " AND (
+//                     {$wpdb->posts}.post_title LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.post_content LIKE '{$search_word}'
+//                     OR {$wpdb->posts}.ID IN (
+//                     SELECT distinct post_id
+//                     FROM {$wpdb->postmeta}
+//                     WHERE meta_value LIKE '{$search_word}')) ";
+//             }
+//         }
+//     }
+//     return $search;
+// }
