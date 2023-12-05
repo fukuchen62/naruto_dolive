c2b4619e17dc1c5aba959527320a84814a6f84a5
myossy94 <cocolunlun.m@gmail.com>
2023-12-05 16:02:18 +0900

    Update course_color.js

--- assets/js/course_color.js
+++ assets/js/course_color.js
@@ -7,22 +7,22 @@ const recTitles = document.querySelectorAll(".rec_title");
 
 if (courseTitle === "鳴門海峡満喫旅") {
     courseCards.forEach((courseCard) => {
-        courseCard.style.backgroundColor = "#FF8B03";
-        });
+        courseCard.style.backgroundColor = "#FF8B0380";
+    });
 } else if (courseTitle === "歴史・文化の鳴門旅") {
     courseCards.forEach((courseCard) => {
-        courseCard.style.backgroundColor = "#996633";
+        courseCard.style.backgroundColor = "#99663380";
     });
 } else if (courseTitle === "ぐるっと一周鳴門旅") {
     courseCards.forEach((courseCard) => {
-        courseCard.style.backgroundColor = "#FF1E1E";
+        courseCard.style.backgroundColor = "#FF1E1E80";
     });
 } else if (courseTitle === "鳴門おおそと一周旅") {
     courseCards.forEach((courseCard) => {
-        courseCard.style.backgroundColor = "#11CD7E";
+        courseCard.style.backgroundColor = "#11CD7E80";
     });
 } else if (courseTitle === "キッズが主役旅") {
     courseCards.forEach((courseCard) => {
-        courseCard.style.backgroundColor = "#D427F0";
+        courseCard.style.backgroundColor = "#D427F080";
     });
 }
